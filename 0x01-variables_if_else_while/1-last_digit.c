1fee96e17e48cc6bc3f6eaa55411f92ab6786c9d
Abdulbasit <abdulbasit.oa7@gmail.com>
2023-10-04 09:57:37 -0700

    corrections... sighgit add .

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Desription: executing another if...else statement
+ *
+ * Return: Always 0 (success)
+ */
+/* betty sytle doc for funtion main goes there */
+int main(void)
+
+
+
