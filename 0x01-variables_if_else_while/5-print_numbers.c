edcbc2b4409e71383fa43d09924689286b5d901a
Abdulbasit <abdulbasit.oa7@gmail.com>
2023-10-25 10:53:29 -0700

    one sucker punch coming up

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - executing a loop
+ *
+ * Return - Always 0 (success)
+ */
+int main(void)
+
+{
+
+	{
+		printf("0123456789");
+	}
+
+	{
+		printf("\n");
+	}
+
+	return (0);
+}
+
+
+
