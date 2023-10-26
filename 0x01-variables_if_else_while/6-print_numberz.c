23a822e194703e1eed9a45627cabc35d27123f0d
Abdulbasit <abdulbasit.oa7@gmail.com>
2023-10-25 19:27:32 -0700

    Oh Allah!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: executing loop with putchar only
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char con;
+
+	for (con = '0'; con <= '9'; con++)
+	{
+		putchar(con);
+	}
+
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
