406df3cd0dea666735e73214e4c861d219e52dbb
Abdulbasit <abdulbasit.oa7@gmail.com>
2024-02-01 07:10:24 -0800

    printing a-z applying user-defined header files; learnt what files to include in my gcc compiler

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#if header
+	#include "main.h"
+#elif header2
+	#include "main2.h"
+#endif
+/**
+ * main - Entry point
+ *
+ * Description: working with header files(printing a-z)
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char ichi;
+
+	for (ichi = 'a'; ichi <= 'z'; ichi++)
+	{
+		putchar(ichi);
+	}
+
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,6 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void);
+
