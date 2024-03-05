fdf48b7d2e27d854241d46399b56327f9706e450
Abdulbasit <abdulbasit.oa7@gmail.com>
2024-03-05 06:14:42 -0800

    prints two integers and returns the result (using only the standard library: <stdio.h> and a user-defined library)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result to stdout.
+ * @a: The character to print.
+ * @b: The character to print.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
